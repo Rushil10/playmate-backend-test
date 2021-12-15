112a4de0d88c4ba9ba8d030e9498c08adb4c3cff
Rushil10 <rushil.10.shah@gmail.com>
2021-12-15 11:23:54 +0530

    notify

--- controllers/notification.js
+++ controllers/notification.js
@@ -1,6 +1,8 @@
-//var FCM = require('fcm-node');
-import FCM from "fcm-node";
-var serverKey = 'AAAAD7aYqkM:APA91bFChWAZYsgKlTQ7-MCiCg1udPMFVu9OQx2Fr6xEN4g8yFNPtFob5rKLmDfpgcLNkAbR7pfK5b9PXtZ2L1nuN-y44FJTlDhjew-P-nM7v17A42RX1OUgrMr4xnJguJ9SWfnCEMXd'; //put your server key here
+
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+var FCM = require('fcm-node')
+var serverKey = require('../playmate-privatekey.json')
 var fcm = new FCM(serverKey);
 
 export const testNotification = (req, res) => {
@@ -8,27 +10,22 @@ export const testNotification = (req, res) => {
     var title = req.body.title
     var body = req.body.body
     var rtoken = req.body.fcmToken
-    var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
-        //to: rtoken,
-        //collapse_key: 'your_collapse_key',
-        registration_ids:[rtoken],
-        //token: rtoken,
+    var message = {
+        registration_ids: [rtoken],
         notification: {
             title: title,
             body: body
         },
-
-        /* data: {  //you can send only notification or only data(or include both)
-            my_key: 'my value',
-            //my_another_key: 'my another value'
-        } */
     };
     console.log(message)
     fcm.send(message, function (err, response) {
         if (err) {
-            console.log("Something has gone wrong!",err);
+            console.log("Something has gone wrong!", err);
         } else {
             console.log("Successfully sent with response: ", response);
+            if (response.results) {
+                console.log(response.results[0])
+            }
         }
     });
 }
