4835e6c3c37271d76e17658612145efec9eebbeb
Rushil10 <rushil.10.shah@gmail.com>
2021-12-15 13:35:10 +0530

    fix

--- controllers/players.js
+++ controllers/players.js
@@ -137,7 +137,7 @@ export const addWebFcmToken = async (req, res) => {
   var playerId = req.player._id
   try {
     await player.findOneAndUpdate({ _id: playerId }, {
-      $addToSFet: {
+      $addToSet: {
         webFcmToken: req.body.fcmToken
       }
     })
